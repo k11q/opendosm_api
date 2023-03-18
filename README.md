a3a9c0e498f8392813bf13e839e4cc019fe283ee
Khairul Haaziq <rhycoz@yahoo.com>
2023-03-19 06:19:58 +0800

    add

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# OpenDOSM Unofficial Public API
+
 This is a public API of openDOSM datasets:
 
 Base url:
@@ -6,6 +8,15 @@ Base url:
 https://opendosmapi-production.up.railway.app/api/
 ```
 
+There is ``bucket`` and ``file_name``
+
+The api works this way:
+
+```
+https://opendosmapi-production.up.railway.app/api/${bucket}/${file_name}
+```
+
+## Example
 
 Say the data you want is located in this csv:
 
@@ -32,3 +43,33 @@ returns the columns for only
 - series_type
 - date
 - ron97
+
+## Data From Aksara Data
+
+Datasets from Aksara Data repo are also available(which includes metadata, variables, etc...)
+
+The API behaves like the repo data structure, where theres ``catalog`` and ``data``.
+
+This is how they are structured:
+
+catalog (basename)
+ | data1.json (file_name)
+ | data2.json (file_name)
+
+data (basename)
+ | data1.json (file_name)
+ | data2.json (file_name)
+
+The API works this way:
+
+```
+https://opendosmapi-production.up.railway.app/api/${basename}/${file_name}
+```
+
+Example:
+
+for getting the ``/catalog/data1.json`` json:
+
+```
+https://opendosmapi-production.up.railway.app/api/catalog/data1
+```
--- index.js
+++ index.js
@@ -1,51 +1,83 @@
-const express = require('express');
-const axios = require('axios');
-const Papa = require('papaparse');
+const express = require("express");
+const axios = require("axios");
+const Papa = require("papaparse");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 function filterColumns(data, columns) {
-  return data.map((row) => {
-    const newRow = {};
-    columns.forEach((column) => {
-      if (row.hasOwnProperty(column)) {
-        newRow[column] = row[column];
-      }
-    });
-    return newRow;
-  });
+	return data.map((row) => {
+		const newRow = {};
+		columns.forEach((column) => {
+			if (row.hasOwnProperty(column)) {
+				newRow[column] = row[column];
+			}
+		});
+		return newRow;
+	});
 }
 
-app.get('/api/:basename/:filename', async (req, res) => {
-  try {
-    const { basename, filename } = req.params;
-    const { columns } = req.query;
+app.set("json spaces", 4);
 
-    const response = await axios.get(`https://storage.googleapis.com/${basename}/${filename}.csv`);
+app.get("/api/:bucket/:file_name", async (req, res) => {
+	try {
+		const { bucket, file_name } = req.params;
+		const { columns } = req.query;
 
-    Papa.parse(response.data, {
-      header: true,
-      complete: (results) => {
-        let filteredResults = results.data;
+		if (bucket == "catalog" || bucket == "dashboard") {
+			axios.get(
+				`https://raw.githubusercontent.com/dosm-malaysia/aksara-data/main/${bucket}/${file_name}.json`
+			)
+				.then(function (response) {
+					res.setHeader(
+						"Content-Type",
+						"application/json"
+					);
+					res.send(response.data);
+				})
+				.catch(function (error) {
+					res.status(500).json({
+						message: `Error fetching JSON file :${bucket}/${file_name}`,
+						error,
+					});
+				});
+		} else {
+			const response = await axios.get(
+				`https://storage.googleapis.com/${bucket}/${file_name}.csv`
+			);
 
-        if (columns) {
-          const columnList = columns.split(',');
-          filteredResults = filterColumns(filteredResults, columnList);
-        }
+			Papa.parse(response.data, {
+				header: true,
+				complete: (results) => {
+					let filteredResults = results.data;
 
-        res.json(filteredResults);
-      },
-      error: (err) => {
-        res.status(500).json({ message: `Error parsing CSV file :${basename}/${filename}`, error: err });
-      },
-    });
-  } catch (error) {
-    res.status(500).json({ message: `Error fetching CSV file: :${basename}/${filename}`, error });
-  }
+					if (columns) {
+						const columnList =
+							columns.split(",");
+						filteredResults = filterColumns(
+							filteredResults,
+							columnList
+						);
+					}
+
+					res.json(filteredResults);
+				},
+				error: (err) => {
+					res.status(500).json({
+						message: `Error parsing CSV file :${bucket}/${file_name}`,
+						error: err,
+					});
+				},
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: `Error fetching CSV file: :${bucket}/${file_name}`,
+			error,
+		});
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
-
