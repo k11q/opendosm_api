da16afb18f6242d330b2b27459ba829943e26e6a
Khairul Haaziq <rhycoz@yahoo.com>
2023-03-19 10:21:51 +0800

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const Papa = require("papaparse");
-const cors = require("corse");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
